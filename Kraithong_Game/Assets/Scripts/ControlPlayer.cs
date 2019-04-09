5190899c3023044f966c1765757a5982aa896b5e
FAHKRAM <59070002@it.kmitl.ac.th>
2019-04-09 12:22:17 +0700

    move & Score game

---
+++ Kraithong_Game/Assets/Scripts/ControlPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPlayer : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(speed * moveX, rb.velocity.y);
+
+        float moveY = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(speed * moveY, rb.velocity.x);
+    }
+}
