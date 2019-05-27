753de5ba39badb81fe247eec057a707a8d03f50d
ITSupapitch <59070169@it.kmitl.ac.th>
2019-05-27 15:51:02 +0700

    Add Enemy Spawner Level.1

---
+++ Kraithong_Game/Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float min_y = -4.3f, max_y = 4.3f;
+
+    public GameObject[] obst_prefabs;
+
+    public float timer = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnEnemies", timer);
+    }
+
+    void SpawnEnemies()
+    {
+        float pos_y = Random.Range(min_y, max_y);
+        Vector3 temp = transform.position;
+        temp.y = pos_y;
+
+        if(Random.Range(0,2) > 0)
+        {
+            Instantiate(obst_prefabs[Random.Range(0, obst_prefabs.Length)],
+            temp, Quaternion.identity);
+        }
+
+        Invoke("SpawnEnemies", timer);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
