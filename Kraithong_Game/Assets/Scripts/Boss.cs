dfcc9751af228d2555c57993275525ddcbe15039
KMNIT14 <59070002@it.kmitl.ac.th>
2019-05-27 17:35:38 +0700

    ยิงบอสได้แล้วจ้า

---
+++ Kraithong_Game/Assets/Scripts/Boss.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boss : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        HealthBarScript.health -= 10f;
+    }
+}
---
+++ Kraithong_Game/Assets/Scripts/HealthBarScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+    Image healthBar;
+    float maxHealth = 1000f;
+    public static float health;
+
+    void Start()
+    {
+        healthBar = GetComponent<Image>();
+        health = maxHealth;
+    }
+
+    void Update()
+    {
+        healthBar.fillAmount = health / maxHealth;
+    }
+}
