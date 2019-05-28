6bab3ce4a98226ee55b2067a7fe163c84457bde4
ITSupapitch <59070169@it.kmitl.ac.th>
2019-05-28 09:29:27 +0700

    Add Items Spawner all Level

---
+++ Kraithong_Game/Assets/Scripts/ItemScript/ItemScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    public float speed = 5f;
+    private bool canMove = true;
+
+    public float area_x = -11f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (canMove)
+        {
+            Vector3 temp = transform.position;
+            temp.x -= speed * Time.deltaTime;
+            transform.position = temp;
+
+            if (temp.x < area_x)
+                gameObject.SetActive(false);
+
+        }
+    }
+}
---
+++ Kraithong_Game/Assets/Scripts/ItemScript/ItemSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public float min_y = -4.3f, max_y = 4.3f;
+
+    public GameObject[] item_prefabs;
+
+    public float timer = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnItems", timer);
+    }
+
+    void SpawnItems()
+    {
+        float pos_y = Random.Range(min_y, max_y);
+        Vector3 temp = transform.position;
+        temp.y = pos_y;
+
+        if (Random.Range(0, 2) > 0)
+        {
+            Instantiate(item_prefabs[Random.Range(0, item_prefabs.Length)],
+            temp, Quaternion.identity);
+        }
+
+        Invoke("SpawnItems", timer);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
