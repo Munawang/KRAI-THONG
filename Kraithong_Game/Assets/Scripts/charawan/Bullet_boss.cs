55630c1ad00a2374bb1e2bb9cf1dd63abb092457
KMNIT14 <59070002@it.kmitl.ac.th>
2019-05-28 14:50:22 +0700

    ชาละวันปล่อยพลังด่าน 3

---
+++ Kraithong_Game/Assets/Scripts/charawan/Bullet_boss.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_boss : MonoBehaviour
+{
+    private Vector2 MoveDirection;
+    private float moveSpeed;
+
+    // Start is called before the first frame update
+    void OnEnable(){
+        Invoke("Destroy", 3f);
+    }
+
+    // Update is called once per frame
+    void Start(){
+        moveSpeed = 5f;
+    }
+
+    void Update(){
+        transform.Translate(MoveDirection * moveSpeed * Time.deltaTime);
+    }
+    public void setMoveDirection(Vector2 dir) {
+        MoveDirection = dir;
+    }
+    private void Destroy(){
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+}
